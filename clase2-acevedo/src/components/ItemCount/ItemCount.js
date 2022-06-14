4992d0e30a25f092080f7c33fbe414e609dc5d4a
Nicolas Acevedo <nicoacevedocotiza@gmail.com>
2022-06-14 13:04:43 -0300

    count

---
+++ clase2-acevedo/src/components/ItemCount/ItemCount.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import './ItemCountStyle.css';
+
+export const ItemCount = ({ stock = "5", initial = "1"}) => {
+    const [count, setCount] = useState(initial);
+
+    const onAdd = () => { 
+        if (count < stock) {
+          setCount(count + 1);
+        }
+    }
+    const onSubs = () => { 
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    }
+
+  return (
+    <div className="ItemCountContainer">
+        <div>
+            <button  className="button button3" onClick={onSubs}>-</button>
+            <span>{count}</span>
+            <button  className="button button1" onClick={onAdd}>+</button>
+        </div>
+        <div>
+            <button className="button" >Agregar al carrito</button>
+        </div>
+    </div>
+  )
+}
+
--- clase2-acevedo/src/components/ItemListContainer/ItemListContainer.js
+++ clase2-acevedo/src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { ItemCount } from "../ItemCount/ItemCount";
 import './ItemListContainer.css';
 
 export const ItemListContainer = (props) => {
   return (
       <div className="ItemListContainer">
         <h2 style={{textAlign:'center'}}>{props.gretting}</h2>
+        <div className="Container">
+          <ItemCount/>
+        </div>
       </div>
   );
 }
