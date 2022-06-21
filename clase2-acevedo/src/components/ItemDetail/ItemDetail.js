dd620f5ba8539d2c30b2b0a148f63ed701a49d03
Nicolas Acevedo <nicoacevedocotiza@gmail.com>
2022-06-21 17:25:50 -0300

    imgDetail

--- clase2-acevedo/src/components/ItemDetail/ItemDetail.js
+++ clase2-acevedo/src/components/ItemDetail/ItemDetail.js
@@ -8,19 +8,18 @@ export const ItemDetail = ({imagen, nombre, descripcion, precio}) => {
           return (
             <div className='card'>
               <div className='imgContainer'>
-                <img src={imagen} alt="repuesto"/>
+                <img src={imagen} alt="repuesto"/> 
               </div>
 
               <div className='descriptionContainer'>
                 <h1>{nombre}</h1>
-                <p> $ {precio}</p>
+                <p className='productPrice'> $ {precio}</p>
                 <p>{descripcion}</p>
                 <ItemCount/>
                 <div>
-                  <p>Las piezas electricas poseen una garantia</p>
+                  <p className='warranty'>Las piezas electricas poseen una garantia de 6 meses</p>
                 </div>
               </div>
-              
             </div>
           );
         };
