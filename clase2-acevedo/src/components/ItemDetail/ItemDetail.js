58d7c24eaa59a8f3287acfe3c9c3b8efa330f9c9
Nicolas Acevedo <nicoacevedocotiza@gmail.com>
2022-06-23 23:53:44 -0300

    primer entrega  del proyecto final

--- clase2-acevedo/src/components/ItemDetail/ItemDetail.js
+++ clase2-acevedo/src/components/ItemDetail/ItemDetail.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import './ItemDetail.css';
+import { useParams } from 'react-router-dom'
 import { ItemCount } from "../ItemCount/ItemCount";
 
-
-export const ItemDetail = ({imagen, nombre, descripcion, precio}) => {
-
+export const ItemDetail = (productos) => {
+  const { productById } = useParams()
+console.log(productById);
+  
+  const findElem = (productos) => { 
+    const elem = productos.find(e => e.id === productById)
+    console.log(elem)
+  }
           return (
             <div className='card'>
               <div className='imgContainer'>
-                <img src={imagen} alt="repuesto"/> 
+                <img src={findElem.imagen} alt="repuesto"/> 
               </div>
 
               <div className='descriptionContainer'>
-                <h1>{nombre}</h1>
-                <p className='productPrice'> $ {precio}</p>
-                <p>{descripcion}</p>
+                <h1>{findElem.nombre}</h1>
+                <p className='productPrice'> $ {findElem.precio}</p>
+                <p>{findElem.descripcion}</p>
                 <ItemCount/>
                 <div>
                   <p className='warranty'>Las piezas electricas poseen una garantia de 6 meses</p>
--- clase2-acevedo/src/components/ItemDetailContainer/ItemDetailContainer.js
+++ clase2-acevedo/src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState} from 'react'
-import axios from "axios"
-import { ItemDetail } from '../ItemDetail/ItemDetail';
+import React, { useEffect, useState } from 'react'
+import { ItemDetail } from '../ItemDetail/ItemDetail'
 
+const ItemDetailContainer = () => {
 
- const ItemDetailContainer = () => {
-      const [item, setItem] = useState([]);
-    
-      useEffect(() => {
-        const getItems = () => {
-          axios.get("http://localhost:3000/data/products.json")
-            .then((response) => setItem(response.data.item));
-console.log(item);
-        };
-        getItems();
-      }, []);
-console.log("hola 1");
+const [productos, setProductos] = useState([])
 
-      return (
-        <ItemDetail item ={item}/>
-      );
-    };
- 
-    export default ItemDetailContainer
-   
-    
-    
+useEffect(() => {
+  async function getItems() {
+    const URL = 'http://localhost:3000/data/products.json' 
+      await fetch(URL)
+          .then( res => res.json() )
+          .then( data => setProductos(data))
+         console.log(productos)
+  }
+    getItems()
+
+}, [productos])
+
+  return (
+    <ItemDetail item ={productos}/>
+  )
+
+}
+
+export default ItemDetailContainer
