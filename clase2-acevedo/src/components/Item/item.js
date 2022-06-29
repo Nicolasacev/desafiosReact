fcd6c9b2770bf6dc75e09f1ba0a60ea04c672bb8
Nicolas Acevedo <nicoacevedocotiza@gmail.com>
2022-06-29 20:18:18 -0300

    arreglo camelcase

--- clase2-acevedo/src/components/Item/item.js
+++ clase2-acevedo/src/components/Item/item.js
@@ -1,18 +1,17 @@
-    import React from 'react'
-    import './ItemStyle.css';
-    import { Link } from 'react-router-dom';
+import React from 'react'
+import './ItemStyle.css';
+import { Link } from 'react-router-dom';
 
-    
-    export const Item = ({imagen, nombre, precio, id}) => {
-      return (
-        <div className='card'>
-          <div className='imgContainer'>
-            <img src={imagen} alt="repuesto"/>
-          </div>
-          <h1>{nombre}</h1>
-          <p> $ {precio}</p>
-          <Link to={`/item/${id} `}className='button'>Ver detalle</Link> 
-        </div>
-      )
-    }
-    
+
+export const Item = ({imagen, nombre, precio, id}) => {
+  return (
+    <div className='card'>
+      <div className='imgContainer'>
+        <img src={imagen} alt="repuesto"/>
+      </div>
+      <h1>{nombre}</h1>
+      <p> $ {precio}</p>
+      <Link to={`/item/${id} `}className='button'>Ver detalle</Link> 
+    </div>
+  )
+}
